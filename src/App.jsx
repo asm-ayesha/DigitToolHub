39c10680b455ddafb728c98f9389fa1b9dc915bd
asm-ayesha <ayesha219333@gmail.com>
2026-03-30 12:28:25 +0600

    added google fonts daisyUi install and change fevicons

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className='text-7xl font-bold text-amber-600'>hello</h1>
-
+      <button className='btn btn-error'>clickj</button>
       
     </>
   )
