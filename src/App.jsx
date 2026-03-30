376ede3e86a8a1dd7da9a823317e4a7aeaa31007
asm-ayesha <ayesha219333@gmail.com>
2026-03-30 11:53:48 +0600

    Initial project Setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-7xl font-bold text-amber-600'>hello</h1>
+
+      
+    </>
+  )
+}
+
+export default App
